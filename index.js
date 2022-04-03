033ce8b149c6c4d8e79aee51c85c257849c5e4ec
Ayushkumar1508 <87596765+Ayushkumar1508@users.noreply.github.com>
2022-04-04 01:22:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ const player = new Fighter({
             framesMax: 6,
         },
         takeHit: {
-            imageSrc: './img/samuraiMack/Take hit - white silhouette.png',
+            imageSrc: './img/samuraiMack/Take Hit - white silhouette.png',
             framesMax: 4
         },
         death: {
